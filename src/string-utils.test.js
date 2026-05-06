4fd56768ffa7ab5dea8bc1b9a9c0bc3a76acddd8
Manoon Pinto <manoonpinto@MacBook-Air-de-Manoon.local>
2026-05-06 09:52:32 +0200

    RED: test reverse() - fails, implementation does not exist yet

---
+++ src/string-utils.test.js
@@ -0,0 +1,13 @@
+import { reverse } from './string-utils';
+
+test('reverse "abc" returns "cba"', () => {
+  expect(reverse('abc')).toBe('cba');
+});
+
+test('reverse "" returns ""', () => {
+  expect(reverse('')).toBe('');
+});
+
+test('reverse(null) throws an error', () => {
+  expect(() => reverse(null)).toThrow('Input must be a string');
+});
