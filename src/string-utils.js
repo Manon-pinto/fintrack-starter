aecc4f69a3e291c31fd9bc0eeed63f9fec56e4cf
Manoon Pinto <manoonpinto@MacBook-Air-de-Manoon.local>
2026-05-06 09:57:37 +0200

    GREEN: implement truncate() - all 4 tests pass

--- src/string-utils.js
+++ src/string-utils.js
@@ -1,3 +1,8 @@
+export function truncate(input, length) {
+  if (input.length <= length) return input;
+  return input.slice(0, length) + '...';
+}
+
 export function reverse(input) {
   if (typeof input !== 'string') {
     throw new Error('Input must be a string');
